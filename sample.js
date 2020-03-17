5ffe46d811bee56a6c7dd370e915c3f27bcf50eb
Valentin Semirulnik <v7rulnik@gmail.com>
2020-03-17 13:02:46 +0300

    add sample.js with tabs

---
+++ sample.js
@@ -0,0 +1,11 @@
+function areInputsEqual(newInputs, lastInputs) {
+	if (newInputs.length !== lastInputs.length) {
+		return false;
+	}
+	for (let i = 0; i < newInputs.length; i++) {
+		if (newInputs[i] !== lastInputs[i]) {
+			return false;
+		}
+	}
+	return true;
+}
